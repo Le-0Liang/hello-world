88811628ffc3a7e92ffbb1d14454d4eeb82dde01
Leo <lel144@pitt.edu>
2024-01-11 21:47:59 -0500

    Added Java test file.

---
+++ Testjava.java
@@ -0,0 +1,11 @@
+public class Testjava{
+    public static void main(String args[]){
+
+
+        int x = 1;
+        int y = 2;
+
+        int sum = x + y;
+        System.out.println(sum);
+    }
+}
